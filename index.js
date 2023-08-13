06c74128b171945dca60fb52e47a6f390b79a552
Pedro Serpa <140448079+pedroserpag@users.noreply.github.com>
2023-08-13 11:29:45 -0300

    add JS
    add JavaScript

---
+++ index.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault;
+
+  CheckUsername();
+});
+
+function errorInput(input, messege) {
+  const formItem = input.parentElement;
+  const textMessege = formItem.querySelector("p");
+
+  textMessege.innerText = messege;
+  formItem.ClassName = "input-box error";
+}
+
+function CheckUsername() {
+  const usernameValue = username.value;
+  if (usernameValue === " ") {
+    errorInput(username, "Este campo é obrigatório");
+  } else {
+    const formItem = username.parentElement;
+    formItem.ClassName = "input-box";
+  }
+}
