5add01d70a2573d5553d55e9a32e108e4350dd2c
Pedro Serpa <140448079+pedroserpag@users.noreply.github.com>
2023-08-15 10:06:09 -0300

    validation form

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const password = document.getElementById("password");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  CheckUsername();
-  CheckPassword();
+  checkForm();
 });
 
 function errorInput(input, messege) {
@@ -17,22 +16,43 @@ function errorInput(input, messege) {
   formItem.className = "input-box error";
 }
 
-function CheckUsername() {
+function CheckInputUsername() {
   const usernameValue = username.value;
   if (usernameValue === "") {
-    errorInput(username, "Este campo é obrigatório");
+    errorInput(username, "Digite um username!");
   } else {
     const formItem = username.parentElement;
     formItem.className = "input-box";
   }
 }
 
-function CheckPassword() {
+function CheckInputPassword() {
   const passwordValue = password.value;
   if (passwordValue === "") {
-    errorInput(password, "Este campo é obrigatório");
+    errorInput(password, "Digite uma password!");
   } else {
     const formItem = password.parentElement;
     formItem.className = "input-box";
   }
 }
+
+function checkForm() {
+  CheckInputUsername();
+  CheckInputPassword();
+
+  const formItems = form.querySelectorAll(".input-box");
+  const isValid = [...formItems].every((item) => {
+    return item.className === "input-box";
+  });
+
+  if (isValid) {
+    alert("CADASTRADO COM SUCESSO");
+  }
+}
+
+username.addEventListener("blur", () => {
+  CheckInputUsername();
+});
+password.addEventListener("blur", () => {
+  CheckInputPassword();
+});
